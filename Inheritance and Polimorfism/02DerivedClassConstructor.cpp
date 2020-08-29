ed3ea3e5863685a08b2bd19a2e2972be9bb57f8e
Renzodef <renzodefrancesco@gmail.com>
2020-08-29 11:11:13 +0200

    29 August 2020

---
+++ Inheritance and Polimorfism/02DerivedClassConstructor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Mother
+{
+public:
+    Mother()
+    {
+        cout << "Mother ctor" << endl;
+    }
+    ~Mother()
+    {
+        cout << "Mother dtor";
+    }
+};
+
+class Daughter : public Mother
+{
+public:
+    Daughter()
+    {
+        cout << "Daughter ctor" << endl;
+    }
+    ~Daughter()
+    {
+        cout << "Daughter dtor" << endl;
+    }
+};
+
+int main()
+{
+    Daughter m;
+}
